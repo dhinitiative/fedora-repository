49eed37e9148be6b3097a68d270ea682c3a015bb
Steve Young <slyoung@hamilton.edu>
2015-12-18 14:28:26 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -24,10 +24,10 @@ export SERVER_FULL_NAME=`hostname -f`                   # set server full name
 export ADMIN_EMAIL='youremail@domain.com'               # enter your email address to get messages from the server
 export INSTALL_PREFIX=`pwd`
 export INSTALL_DIR=/opt
-#export WEB_ROOT=/var/www/html
-export IP_ADDR="xxx.xxx.xxx.xxx"                        # IP address of this server (ifconfig -a). 
-#export SITES_ALL_MODULES=$WEB_ROOT/sites/all/modules
-#export SITES_ALL_LIB=$WEB_ROOT/sites/all/libraries
+export IP_ADDR=`hostname -i`                        # IP address of this server (ifconfig -a). 
+#
+# Software versions
+#
 export FEDORA_VERSION="3.8"                             # Valid versions 3.5 and 3.8
 export SOLR_VERSION="4.2.0"                             # Valid versions 3.6.2 and 4.2.0
 export GSEARCH_VERSION="2.6"	                        # Valid versions 2.4.2 and 2.6
@@ -40,7 +40,6 @@ export FEDORA_USER_PASS='fedora2014'
 export REPOSITORY_HOME=$INSTALL_PREFIX
 export SOLR_HOME=$INSTALL_PREFIX/solr
 export JAVA_HOME=${INSTALL_PREFIX}/java
-#export DOC_ROOT=/var/www/html
 #
 ## Variables to be passed to database.sh
 #
